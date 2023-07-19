18025c2126a94c2cf2403cc15325831d95ffe216
S-furi <stefano.furi@studio.unibo.it>
2023-07-19 16:32:26 +0200

    Added basic operations for testing

--- src/jvmMain/kotlin/me.stefanofuri.application/server/schema/queries/TestQuery.kt
+++ src/jvmMain/kotlin/me.stefanofuri.application/server/schema/queries/TestQuery.kt
@@ -1,2 +1,7 @@
 package me.stefanofuri.application.server.schema.queries
 
+import com.expediagroup.graphql.server.operations.Query
+
+class TestQuery : Query {
+    fun test() = Double.MAX_VALUE
+}
--- src/jvmMain/kotlin/me.stefanofuri.application/server/schema/subscriptions/PointSubscription.kt
+++ src/jvmMain/kotlin/me.stefanofuri.application/server/schema/subscriptions/PointSubscription.kt
@@ -1,2 +1,19 @@
 package me.stefanofuri.application.server.schema.subscriptions
 
+import com.expediagroup.graphql.generator.annotations.GraphQLDescription
+import com.expediagroup.graphql.server.operations.Subscription
+import me.stefanofuri.application.server.schema.models.Point
+import kotlin.random.Random
+
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.delay
+
+class PointSubscription : Subscription {
+    @GraphQLDescription("Returns a random 2D point every 500 milliseconds")
+    fun randomPoints(limit: Int? = null) = flow {
+        (0..(limit ?: Int.MAX_VALUE)).forEach { _ ->
+            emit(Point(Random.nextInt(), Random.nextInt()))
+            delay(500)
+        }
+    }
+}
