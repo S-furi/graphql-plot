ce2757ef5f75fc2cccceddf4d03b017330f1dd45
S-furi <stefano.furi@studio.unibo.it>
2023-07-21 14:35:30 +0200

    Added base project build.gradle file

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    alias(libs.plugins.kotlinMultiplatformPlugin)
+    alias(libs.plugins.apollographqlPlugin)
+    alias(libs.plugins.ktorPlugin)
+    alias(libs.plugins.graphqlServerPlugin)
+}
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    sourceSets {
+        jvm() {
+
+        }
+
+        js(IR) {
+            binaries.executable()
+            browser {
+            }
+        }
+
+        val jsMain by getting {
+            kotlin.srcDir("js-app/src")
+
+            dependencies {
+                implementation(libs.apollo.runtime)
+                implementation(libs.logback)
+                implementation(libs.letsplot.js)
+                implementation(libs.kotlin.coroutines.core)
+            }
+
+        }
+    }
+}
