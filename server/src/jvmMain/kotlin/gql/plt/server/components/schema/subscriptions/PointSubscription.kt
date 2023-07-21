10cfb930bdb43e8639683cd11e072594bdba3a41
S-furi <stefano.furi@studio.unibo.it>
2023-07-21 16:04:28 +0200

    Limiting random number for point's coordinates

--- server/src/jvmMain/kotlin/gql/plt/server/components/schema/subscriptions/PointSubscription.kt
+++ server/src/jvmMain/kotlin/gql/plt/server/components/schema/subscriptions/PointSubscription.kt
@@ -12,7 +12,7 @@ class PointSubscription : Subscription {
     @GraphQLDescription("Returns a random 2D point every 500 milliseconds")
     fun randomPoints(limit: Int? = null) = flow {
         (0..(limit ?: Int.MAX_VALUE)).forEach { _ ->
-            emit(Point(Random.nextInt(), Random.nextInt()))
+            emit(Point(Random.nextInt(100), Random.nextInt(100)))
             delay(500)
         }
     }
