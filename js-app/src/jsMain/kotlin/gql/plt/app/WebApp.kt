fb7c4b3d910465beda42a608affe918ea41bab17
S-furi <stefano.furi@studio.unibo.it>
2023-07-21 16:30:15 +0200

    Reformatted code

--- js-app/src/jsMain/kotlin/gql/plt/app/WebApp.kt
+++ js-app/src/jsMain/kotlin/gql/plt/app/WebApp.kt
@@ -1,49 +1,45 @@
 package gql.plt.app
 
-import kotlinx.browser.document
-import kotlinx.browser.window
-import kotlinx.coroutines.MainScope
-
-import org.jetbrains.letsPlot.frontend.JsFrontendUtil
-import org.jetbrains.letsPlot.letsPlot
-import org.jetbrains.letsPlot.Figure
-import org.jetbrains.letsPlot.geom.geomPoint
-
-import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.*
-import kotlinx.dom.clear
-
 import com.apollographql.apollo3.api.ApolloResponse
 import com.apollographql.apollo3.api.Optional
 import gql.plt.app.client.DefaultClient
 import jsMain.client.PointsSubscription
+import kotlinx.browser.document
+import kotlinx.browser.window
+import kotlinx.coroutines.*
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.flow.*
+import kotlinx.dom.clear
+import org.jetbrains.letsPlot.Figure
+import org.jetbrains.letsPlot.frontend.JsFrontendUtil
+import org.jetbrains.letsPlot.geom.geomPoint
 import org.jetbrains.letsPlot.geom.geomSmooth
+import org.jetbrains.letsPlot.letsPlot
 
 fun main() {
     window.onload = { createContext() }
 }
 
 fun createContext() {
-    val data = mapOf(
-        "x" to mutableListOf<Int>(),
-        "y" to mutableListOf()
-    )
+    val data = mapOf("x" to mutableListOf<Int>(), "y" to mutableListOf())
 
-    val job = MainScope().launch {
+    MainScope().launch {
         getData()
             .onEach {
                 it.data?.randomPoints?.let { point ->
-                    data["x"]?.add(point.x) ; data["y"]?.add(point.y)
+                    data["x"]?.add(point.x)
+                    data["y"]?.add(point.y)
                 }
                 println(it.data?.randomPoints)
                 addPlotToDiv(getPlot(data))
-            }.onCompletion {
+            }
+            .onCompletion {
                 println("Done Collecting")
                 val finalPlot = getPlot(data) + geomSmooth(deg = 3)
                 addPlotToDiv(finalPlot)
-            }.collect()
+            }
+            .collect()
     }
-
 }
 
 fun addPlotToDiv(p: Figure) {
@@ -53,8 +49,10 @@ fun addPlotToDiv(p: Figure) {
 }
 
 fun getPlot(data: Map<String, Any>, xx: String = "x", yy: String = "y") =
-    letsPlot(data) { x = xx ; y = yy } +
-            geomPoint(color = "red", size = 3.0)
+        letsPlot(data) {
+            x = xx
+            y = yy
+        } + geomPoint(color = "red", size = 3.0)
 
 fun getData(): Flow<ApolloResponse<PointsSubscription.Data>> {
     val apolloClient = DefaultClient.Builder().serverUrl().addSubscriptionModule().build()
